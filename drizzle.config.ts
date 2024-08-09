e1d34add98461fd02fcc6871ad97ded7daa98997
Mathias Lawson <mathiaslawson70@gmail.com>
2024-08-09 11:47:34 +0000

    embedding and resources

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -8,5 +8,5 @@ export default {
   dbCredentials: {
     url: env.DATABASE_URL,
   },
-  tablesFilter: ["klaus_embedded"],
+  tablesFilter: ["mock_*"],
 } satisfies Config;
--- src/server/db_drizzle/schema.ts
+++ src/server/db_drizzle/schema.ts
@@ -9,6 +9,9 @@ import {
   timestamp,
   varchar,
 } from "drizzle-orm/pg-core";
+import { nanoid } from "~/lib/utils";
+import { pgTable, text, vector } from "drizzle-orm/pg-core";
+import { resources } from "./resources";
 
 
 export const createTable = pgTableCreator((name) => `mock_${name}`);
@@ -27,5 +30,27 @@ export const posts = createTable(
   },
   (example) => ({
     nameIndex: index("name_idx").on(example.name),
+  }),
+);
+
+
+export const embeddings = pgTable(
+  "embeddings",
+  {
+    id: varchar("id", { length: 191 })
+      .primaryKey()
+      .$defaultFn(() => nanoid()),
+    resourceId: varchar("resource_id", { length: 191 }).references(
+      () => resources.id,
+      { onDelete: "cascade" },
+    ),
+    content: text("content").notNull(),
+    embedding: vector("embedding", { dimensions: 1536 }).notNull(),
+  },
+  (table) => ({
+    embeddingIndex: index("embeddingIndex").using(
+      "hnsw",
+      table.embedding.op("vector_cosine_ops"),
+    ),
   }),
 );
